1f2215357f6fa91c8cf817f6e49d92e0614da07b
Luiz Henrique Fernandes da Silva <91839356+luiz-henrique-f@users.noreply.github.com>
2025-08-09 00:15:59 -0300

    refactor: I refactored docker-compose because it was buggy

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'postgres',
       port: 5432,
       username: 'postgres',
       password: 'postgres',
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+  // Cria o servidor HTTP (aplicação principal)
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  // Conecta o microsserviço Kafka ao servidor principal
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['kafka:9092'], // Use o endereço do seu broker Kafka
+      },
+      /* consumer: {
+        groupId: 'orders-consumer', // Defina um groupId para o consumidor
+      }, */
+    },
+  });
+
+  // Inicia o microsserviço e o servidor HTTP
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
